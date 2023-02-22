d3b98cfe198bf64088ee8ff6480dc66b36a772aa
harvysdlc <harvysdlc@gmail.com>
2023-02-22 11:05:20 +0800

    update

---
+++ Profile.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class Profile {
+
+    public void signUp(){
+        Scanner scan = new Scanner(System.in);
+        System.out.println("<<<<<<<<<<<<<<<<<<<<<<<<<o>>>>>>>>>>>>>>>>>>>>>>>>>");
+        
+    }
+
+    public void signIn(){
+
+    }
+
+    
+}
